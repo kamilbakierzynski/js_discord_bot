0240492888088023d1ce67f59432e08342ef9eba
Kamil Bakierzynski <bakierzynskikamil@gmail.com>
2020-05-02 22:48:29 +0200

    temp disable ranking

--- commands/ranking.js
+++ commands/ranking.js
@@ -2,8 +2,9 @@ module.exports = {
     name: 'ranking',
     description: 'Display current server activity ranking.',
     execute(client, message) {
-        client.googledb.refreshDbDataAll(client).then(result =>
-            setTimeout(() => client.helpers.displayRanking(client), 500)
-            );
+        // client.googledb.refreshDbDataAll(client).then(result =>
+        //     setTimeout(() => client.helpers.displayRanking(client), 500)
+        //     );
+        message.reply(' disabled.');
     }
 };
