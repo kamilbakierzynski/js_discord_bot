cf6216cba62446f52a212ea2865558cf3c0ed3d9
Kamil Bakierzynski <bakierzynskikamil@gmail.com>
2020-05-09 13:22:57 +0200

    omit bots while counting online members

--- modules/dashboard.js
+++ modules/dashboard.js
@@ -9,7 +9,7 @@ exports.loadData = function loadData(client) {
 
     let usersList = {};
     client.guilds.cache.get('654415996702162984').members.cache.forEach((value, key) => {
-        if (value.voice.selfMute !== undefined && value.voice.channelID !== null) {
+        if (value.voice.selfMute !== undefined && value.voice.channelID !== null && !value.user.bot) {
             usersList = { ...usersList, [key]: {id: key, mute: value.voice.selfMute,
                                                 channelID: value.voice.channelID,
                                                 username: value.nickname || value.user.username }}
